4a08ca3209d81d3556a2504b5df95e7c6be3281c
Davida Marion <davidamarion@gmail.com>
2014-03-25 14:23:33 -0700

    adds taco button

--- app/controllers/location_controller.rb
+++ app/controllers/location_controller.rb
@@ -10,6 +10,14 @@ class LocationController < UIViewController
     @subhead.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 6)
     self.view.addSubview @subhead
 
+    @taco_button =  UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @taco_button.setTitle("Find tacos!", forState:UIControlStateNormal)
+    @taco_button.setTitle("Finding Tacos!", forState:UIControlStateDisabled)
+    @taco_button.sizeToFit
+    @taco_button.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height / 4)
+    self.view.addSubview @taco_button
+
+
 
   end
 end
