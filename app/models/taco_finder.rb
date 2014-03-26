b412fc9bf1091067e373fcec080f4b8e930be6d1
Davida Marion <davidamarion@gmail.com>
2014-03-26 09:39:55 -0700

    parses returned data

--- app/models/taco_finder.rb
+++ app/models/taco_finder.rb
@@ -2,7 +2,8 @@ class TacoFinder
   def self.find(location_hash)
     options = { latitude: location_hash.location.coordinate.latitude, longitude: location_hash.location.coordinate.longitude }
     BW::HTTP.post("http://www.taco911.me/taco.json", { payload: options }) do |response|
-      p response.body.to_str
+      result_data = BW::JSON.parse(response.body.to_str)
+      p result_data
       # for now, pass nil.
     end
   end
