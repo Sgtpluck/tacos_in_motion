307a90304cb8334a11bdd1c9559b0bb5097728d1
Davida Marion <davidamarion@gmail.com>
2014-03-26 12:40:34 -0700

    cleaning up restaurant controller

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -16,7 +16,7 @@ class RestaurantController < UIViewController
     @restaurant_info = UILabel.alloc.init
     @restaurant_info.lineBreakMode = UILineBreakModeWordWrap
     @restaurant_info.numberOfLines = 0
-    @restaurant_info.frame = [[0, 0], [view.frame.size.width, view.frame.size.height + 30]]
+    @restaurant_info.frame = [[0, 0], [view.frame.size.width - 10, view.frame.size.height + 30]]
     @restaurant_info.text = "#{self.restaurant['name']} is the closest taco place! It's located at #{self.restaurant['location']['display_address'].join(" ")}"
     @restaurant_info.sizeToFit
     @restaurant_info.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 4)
