373ac4e47ad352f6ea9d8b31e4e3b178f514f062
Davida Marion <davidamarion@gmail.com>
2014-03-26 11:35:31 -0700

    shows the closest taco place

--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -13,7 +13,16 @@ class RestaurantController < UIViewController
     self.title = self.restaurant["name"]
     self.view.backgroundColor = UIColor.whiteColor
 
-
+    @restaurant_info = UILabel.alloc.init
+    @restaurant_info.lineBreakMode = UILineBreakModeWordWrap
+    @restaurant_info.numberOfLines = 0
+    @restaurant_info.frame = [[0, 0], [view.frame.size.width, view.frame.size.height + 30]]
+    @restaurant_info.text = "#{self.restaurant['name']} is the closest taco place! It's located at #{self.restaurant['location']['display_address'].join(" ")}"
+    @restaurant_info.sizeToFit
+    @restaurant_info.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 4)
+    self.view.addSubview @restaurant_info
 
   end
+
+
 end
