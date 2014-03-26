b06598f0ed87d1354d5be79dd5f13e47589fa267
Davida Marion <davidamarion@gmail.com>
2014-03-26 14:49:55 -0700

    map centers on user!

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -16,8 +16,7 @@ class MapController < UIViewController
   def viewDidLoad
     view.frame = self.view.bounds
 
-    # Center on Brussels.
-    region = MKCoordinateRegionMake(CLLocationCoordinate2D.new(50.85, 4.35), MKCoordinateSpanMake(3.1, 3.1))
+    region = MKCoordinateRegionMake(CLLocationCoordinate2D.new(self.latitude, self.longitude), MKCoordinateSpanMake(3.1, 3.1))
     self.view.setRegion(region)
 
   end
