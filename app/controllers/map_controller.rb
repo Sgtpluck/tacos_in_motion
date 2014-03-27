11e4ce252ff85c18dbea7711960a412d6f55115c
Davida Marion <davidamarion@gmail.com>
2014-03-27 16:17:27 -0700

    shows user location

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,8 +1,9 @@
 class MapController < UIViewController
-  attr_accessor :latitude, :longitude, :location_manager
+  attr_accessor :location, :latitude, :longitude, :location_manager
 
   def initWithLocation(location_manager)
     initWithNibName(nil, bundle:nil)
+    @location = location_manager
     self.latitude = location_manager.location.coordinate.latitude
     self.longitude = location_manager.location.coordinate.longitude
     self
@@ -10,6 +11,7 @@ class MapController < UIViewController
   
   def loadView
     self.view = MKMapView.alloc.init
+    self.view.setShowsUserLocation(true)
     view.delegate = self
   end
 
@@ -18,6 +20,10 @@ class MapController < UIViewController
 
     region = MKCoordinateRegionMake(CLLocationCoordinate2D.new(self.latitude, self.longitude), MKCoordinateSpanMake(0.01, 0.01))
     self.view.setRegion(region)
-
+    # @user = MKPinAnnotationView.alloc.initWithAnnotation(CLLocationCoordinate2D.new(self.latitude, self.longitude), reuseIdentifier:nil)
+    # p @user
+    # @user.canShowCallout = true
+    # @user.animatesDrop = true
+    # @user
   end
 end
--- app/controllers/restaurant_controller.rb
+++ app/controllers/restaurant_controller.rb
@@ -17,20 +17,19 @@ class RestaurantController < UIViewController
     @restaurant_info = UILabel.alloc.init
     @restaurant_info.lineBreakMode = UILineBreakModeWordWrap
     @restaurant_info.numberOfLines = 0
-    @restaurant_info.frame = [[0, 0], [view.frame.size.width - 10, view.frame.size.height + 30]]
+    @restaurant_info.frame = [[0, 0], [view.frame.size.width - 50, view.frame.size.height + 30]]
     @restaurant_info.text = "#{self.restaurant['name']} is the closest taco place! It's located at #{self.restaurant['location']['display_address'].join(" ")}"
     @restaurant_info.sizeToFit
-    @restaurant_info.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 4)
+    @restaurant_info.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height / 3)
     self.view.addSubview @restaurant_info
 
     @map_button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
     @map_button.setTitle("Map it!", forState:UIControlStateNormal)
     @map_button.sizeToFit
-    @map_button.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height / 3)
+    @map_button.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height / 5)
     self.view.addSubview @map_button
 
     @map_button.when(UIControlEventTouchUpInside) do
-      p self.location
         self.display_map(self.location)
     end
 
